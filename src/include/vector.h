9215c5d1d5bd38ab958ec4a7514c25758e92deee
Thomas Lupin <thomas.lupin@epita.fr>
2018-11-10 05:00:35 +0100

    [PHYSICS] added vector.h

---
+++ src/include/vector.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <math.h>
+#include <stdio.h>
+
+struct vec2
+{
+    float x;
+    float y;
+};
+
+struct line
+{
+    struct vec2 p1;
+    struct vec2 p2;
+};
+
+struct vec2 v_sum(struct vec2 a, struct vec2 b);
+struct vec2 v_sub(struct vec2 a, struct vec2 b);
+struct vec2 v_scale(struct vec2 v, double a);
+double v_scalar(struct vec2 a, struct vec2 b);
+
+double v_dist(struct vec2 v);
+struct vec2 v_norm(struct vec2 v);
+
+double v_cross(struct vec2 a, struct vec2 b);
+void v_print(struct vec2 a);
