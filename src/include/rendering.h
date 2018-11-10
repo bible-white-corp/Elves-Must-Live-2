1b39d04c29d68c82f42925339e337a155edffe7c
louis.holleville <louis.holleville@epita.fr>
2018-11-10 02:03:51 +0100

    [SDL] Dynamic display coded

--- src/include/rendering.h
+++ src/include/rendering.h
@@ -5,5 +5,11 @@
 #define WIN_WIDTH 800
 #define WIN_HEIGHT 640
 #define GAME_NAME "Elves Must Live 2"
+#define GRASS_PATH "ressources/blocks/grass.png"
+#define VOID_PATH "ressources/blocks/void.png"
+
 
 void init_sdl(struct game *game);
+void destroy_sdl(struct game *game);
+void load_textures(struct game *game);
+void render_frame(struct game *game);
--- src/sdl_ressources/rendering.c
+++ src/sdl_ressources/rendering.c
@@ -37,12 +37,66 @@ void init_sdl(struct game *game)
 
 void destroy_sdl(struct game *game)
 {
+    SDL_DestroyTexture(game->texture_lib[VOID]);
+    SDL_DestroyTexture(game->texture_lib[GRASS]);
+
     SDL_DestroyRenderer(game->renderer);
     SDL_DestroyWindow(game->window);
     SDL_Quit();
 }
 
+void load_textures(struct game *game)
+{
+    SDL_Surface *grass_surface = IMG_Load(GRASS_PATH);
+    if (!grass_surface)
+        errx("cannot load %s", GRASS_PATH);
+
+    SDL_Surface *void_surface = IMG_Load(VOID_PATH);
+    if (!void_surface)
+        errx("cannot load %s", VOID_PATH);
+
+    SDL_Texture *grass_text = SDL_CreateTextureFromSurface(game->renderer,
+            grass_surface);
+    if (!grass_text)
+        errx("cannot convert grass_surface to SDL_Texture");
+    SDL_FreeSurface(grass_surface);
+
+    SDL_Texture *void_text = SDL_CreateTextureFromSurface(game->renderer,
+            void_surface);
+    if (!grass_text)
+        errx("cannot convert void_surface to SDL_Texture");
+    SDL_FreeSurface(void_surface);
+
+    game->texture_lib[VOID] = void_text;
+    game->texture_lib[GRASS] = grass_text;
+}
+
 void render_frame(struct game *game)
 {
-    
+
+    int imax = WIN_WIDTH / BLOCK_SIZE;
+    int jmax = WIN_HEIGHT / BLOCK_SIZE;
+
+    for (int j = 0; j < jmax; j++)
+    {
+        for (int i = 0; i < imax; i++)
+        {
+            SDL_rect srcrect;
+            SDL_rect dstrect;
+            srcrect.x = 0;
+            srcrect.y = 0;
+            srcrect.w = BLOCK_SIZE;
+            srcrect.h = BLOCK_SIZE;
+
+            dstrect.x = i * BLOCK_SIZE;
+            dstrect.y = j * BLOCK_SIZE;
+            dstrect.w = WIN_WIDTH;
+            dstrect.h = WIN_HEIGHT;
+
+            SDL_RenderCopy(game->renderer,
+                    game->texture_lib[game->map->grid[j][i]],
+                    &srcrect,
+                    &dstrect);
+        }
+    }
 }
