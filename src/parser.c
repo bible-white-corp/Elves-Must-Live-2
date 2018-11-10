5eb37640bb5d557476d4ff669c559c7621bbf59a
Thomas Lupin <thomas.lupin@epita.fr>
2018-11-10 02:13:42 +0100

    [PARSER] Switch char to an enum

--- src/parser.c
+++ src/parser.c
@@ -6,16 +6,27 @@ void map_parse(char *path, struct map *map)
 {
     char cur;
     FILE *f = fopen(path, "r");
-    char **grid = malloc(sizeof(char*) * HEIGHT);
+    enum block **grid = malloc(sizeof(enum block*) * HEIGHT);
     for (int j = 0; j < HEIGHT; j++)
     {
-        grid[j] = malloc(sizeof(char) * WIDTH);
+        grid[j] = malloc(sizeof(enum block) * WIDTH);
         for (int i = 0; i < WIDTH + 1; i++)
         {
             fread(&cur, 1, 1, f);
             if (cur == '\n' || cur == EOF)
                 break;
-            grid[j][i] = cur;
+            switch (cur)
+            {
+                case 'b':
+                    grid[j][i] = VOID;
+                    break;
+                case 'w':
+                    grid[j][i] = GRASS;
+                    break;
+                default:
+                    grid[j][i] = VOID;
+                    break;
+            }
         }
         if (cur == EOF)
             break;
@@ -31,29 +42,4 @@ void map_delete(struct map *map)
         free(map->grid[i]);
     }
     free(map->grid);
-}
-
-
-void map_print(struct map *map)
-{
-    printf("MAP:\n");
-    for (int j = 0; j < HEIGHT; j++)
-    {
-        for (int i = 0; i < WIDTH; i++)
-        {
-            printf("%c", map->grid[j][i]);
-        }
-        printf("\n");
-    }
-}
-
-int main(void)
-{
-    struct map map =
-    {
-        NULL
-    };
-    map_parse("map.eml", &map);
-    map_print(&map);
-    map_delete(&map);
 }
